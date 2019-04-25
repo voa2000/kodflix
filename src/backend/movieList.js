6df6cdf775057dc87373b33fd181cca938aeb12e
voa2000 <voa2000@hotmail.com>
2019-04-25 21:25:22 +0100

    Added template for other movies

--- src/backend/movieList.js
+++ src/backend/movieList.js
@@ -1,5 +1,14 @@
 const movieList = [
-{"id":'us', "title":'us',"synoposis":'Another one'},
-{"id":'us', "title":'us',"synoposis":'Another one'}
+{"id":'mov001', "title":'Us',"cast":"","runningtime":"","director":"","year":"2019","synoposis":'Another one'},
+{"id":'mov002', "title":'Avengers:Endgame',"cast":"","runningtime":"","director":"","year":"2019","synoposis":'Adrift in space with no food or water, Tony Stark sends a message to Pepper Potts as his oxygen supply starts to dwindle. Meanwhile, the remaining Avengers -- Thor, Black Widow, Captain America and Bruce Banner -- must figure out a way to bring back their vanquished allies for an epic showdown with Thanos -- the evil demigod who decimated the planet and the universe.'},
+{"id":'mov003', "title":'',"cast":"","runningtime":"","director":"","year":"2019","synoposis":'',},
+{"id":'mov004', "title":'',"cast":"","runningtime":"","director":"","year":"2019","synoposis":'',},
+{"id":'mov005', "title":'',"cast":"","runningtime":"","director":"","year":"2019","synoposis":'',},
+{"id":'mov006', "title":'',"cast":"","runningtime":"","director":"","year":"2019","synoposis":'',},
+{"id":'mov007', "title":'',"cast":"","runningtime":"","director":"","year":"2019","synoposis":'',},
+{"id":'mov008', "title":'',"cast":"","runningtime":"","director":"","year":"2019","synoposis":'',},
+{"id":'mov009', "title":'',"cast":"","runningtime":"","director":"","year":"2019","synoposis":'',},
+{"id":'mov010', "title":'',"cast":"","runningtime":"","director":"","year":"2019","synoposis":'',}
+
 ]
 module.exports = movieList;
