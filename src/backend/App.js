7d5c8c24ab458aafbb2c6574b31206d9d064dca5
voa2000 <voa2000@hotmail.com>
2019-04-26 09:44:51 +0100

    Changed port Name

--- src/backend/App.js
+++ src/backend/App.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 1000
+const port = 3001
 const movieList = require("./movieList.js");
 
 app.get('/rest/shows', (req, res) => {
