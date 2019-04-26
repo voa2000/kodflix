e9383d7909a69e39556fa7f376e05c968f34bb52
voa2000 <voa2000@hotmail.com>
2019-04-26 12:41:10 +0100

    Port No

--- src/backend/App.js
+++ src/backend/App.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = 3002;
 const movieList = require("./movieList.js");
 
 app.get('/rest/shows', (req, res) => {
