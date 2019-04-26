9d232d749acc7653f711f64513b5bf2a7075d0ce
voa2000 <voa2000@hotmail.com>
2019-04-26 12:14:33 +0100

    Added semi-colons.

--- src/backend/App.js
+++ src/backend/App.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+const app = express();
+const port = 3001;
 const movieList = require("./movieList.js");
 
 app.get('/rest/shows', (req, res) => {
     res.send(movieList);
 })
 
-app.listen(port, () => console.log(`Movie details app listening on port no ${port}!`))
+app.listen(port, () => console.log(`Movie details app listening on port no ${port}!`));
