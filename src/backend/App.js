9b3006c2a448d4d477153b7526d28ad44496870a
voa2000 <voa2000@hotmail.com>
2019-05-08 20:34:49 +0100

    Express linked backend to frontend .Get/Use in app

--- src/backend/App.js
+++ src/backend/App.js
@@ -2,10 +2,17 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 const movieList = require("./movieList.js");
+const path = require('path');
 
 app.get('/rest/shows', (req, res) => {
     res.send(movieList);
 })
+
+app.use(express.static(path.join(__dirname, '../../build')));
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+});
 
 app.listen(port, () => console.log(`Movie details app listening on port no ${port}!`));
 
