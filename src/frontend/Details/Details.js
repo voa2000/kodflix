c3c5c3fa6044aa47177fcce7347663214bf6d240
voa2000 <voa2000@hotmail.com>
2019-06-06 11:58:49 +0100

    Changes to layout

--- src/frontend/Details/Details.js
+++ src/frontend/Details/Details.js
@@ -29,32 +29,32 @@ export default class Details extends React.Component {
     let movies = this.state.moviesData;
     if (movies) {
       return movies.id ? (
-        <div className="App">
-          <div className="details-title">
-            <h1>{this.state.moviesData.title}</h1>
+        <div className="details-container">
+          <div className="details-content">
+            <div className="details-title">
+              <h1>{this.state.moviesData.title}</h1>
+            </div>
           </div>
-          <div className="details-container">
-            <div className="details-text-item">
-              <div className="details-text-title">Synopsis</div>
-              <p>{this.state.moviesData.synopsis}</p>
-              <div className="details-text-title">
-                Year Released:{this.state.moviesData.year}
-              </div>
+          <div className="details-text-item">
+            <div className="details-text-title">Synopsis</div>
+            <div className="details-text">{this.state.moviesData.synopsis}</div>
+            <div className="details-text-title">
+              Year Released: {this.state.moviesData.year}
+            </div>
+            <div className="details-image-item">
               <img
-                className="details-image-item"
                 src={require(`../../frontend/common/images/details/${
                   this.state.moviesData.id
                 }.jpg`)}
                 alt={this.state.moviesData.title}
               />
             </div>
-
-            <Link className="details-back-button" to="/">
-              {" "}
-              Back to home page
-            </Link>
-            <p />
           </div>
+          <Link className="details-back-button" to="/">
+            {" "}
+            Back to Gallery
+          </Link>
+          <p />
         </div>
       ) : (
         <Loading />
